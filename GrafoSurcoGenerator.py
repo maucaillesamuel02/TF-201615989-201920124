a9e838a369dd54f649e3595de1095cf74d028a26
CieloPuntillo <u201920124@upc.edu.pe>
2022-06-22 11:01:09 -0500

    se agrego ruta más corta #6

--- GrafoSurcoGenerator.py
+++ GrafoSurcoGenerator.py
@@ -10,3 +10,7 @@ ox.save_graph_shapefile(prj_road, filepath=r'C:\Users\PERU\Desktop\Test\Santiago
 
 nodes, edges = ox.graph_to_gdfs(G)
 edges_series = edges['length']
+
+G_proj = ox.project_graph(G)
+points = ox.utils_geo.sample_points(ox.get_undirected(G_proj), 10000)
+%time ne2 = ox.nearest_edges(G_proj, X=points.x, Y=points.y, interpolate=10, return_dist=True)
